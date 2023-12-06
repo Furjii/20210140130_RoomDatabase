3157b268324245e4bfc29fcc19c2c229c392f3b3
Furjii <furjimadi@gmail.com>
2023-12-06 16:17:07 +0700

    menyelesaikan code pada AplikasiSiswa.kt

--- app/src/main/java/com/example/roomsiswa/ui/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/ui/AplikasiSiswa.kt
@@ -1,4 +1,19 @@
 package com.example.roomsiswa.ui
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.roomsiswa.repository.ContainerApp
+import com.example.roomsiswa.repository.ContainerDataApp
+
+class AplikasiSiswa: Application() {
+    /*
+    AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+
+
 }
